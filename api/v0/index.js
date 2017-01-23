a6c9f32b0ec3fd54dc52769f40354b1bd86a0576
Al Petersen <al@spotify.com>
2017-01-23 15:41:30 -0500

    test update

--- api/v0/index.js
+++ api/v0/index.js
@@ -12,22 +12,26 @@ module.exports = function getV0Routes(twilioService, yaas) {
   router.post('/sms', (req, res) => {
     console.log(req.body);
 
-    return yaas
-      .answer('does this work right?')
-      .then(yaasResponse => {
-        const text = yaasResponse.text;
-        const gifUrl = yaasResponse.gif.image_url;
+    const testResp = twilioService.createTwimlResponseForMMS("A test message", "http://media0.giphy.com/media/msKNSs8rmJ5m/giphy.gif");
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(testResp);
 
-        return twilioService.createTwimlResponseForMMS(text, gifUrl)
-      })
-      .then(twiml => {
-        console.log('responding with twiml: ', twiml);
-        res.send(twiml);
-      })
-      .catch(err => {
-        console.error(err);
-        res.json(err);
-      });
+    // return yaas
+    //   .answer('does this work right?')
+    //   .then(yaasResponse => {
+    //     const text = yaasResponse.text;
+    //     const gifUrl = yaasResponse.gif.image_url;
+    //
+    //     return twilioService.createTwimlResponseForMMS(text, gifUrl)
+    //   })
+    //   .then(twiml => {
+    //     console.log('responding with twiml: ', twiml);
+    //     res.send(twiml);
+    //   })
+    //   .catch(err => {
+    //     console.error(err);
+    //     res.json(err);
+    //   });
   });
 
   return router;
